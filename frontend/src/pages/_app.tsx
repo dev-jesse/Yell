79fef25180f42828b8eb40b40482cb30c4dfe311
Jesse <83730324+dev-jesse@users.noreply.github.com>
2022-10-25 21:27:04 -0400

    Chakra UI Initial Theme Design

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import { theme } from '../chakra/theme';
 
 function MyApp({ Component, pageProps, }: AppProps<{ session: Session; }>) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   )
 }
