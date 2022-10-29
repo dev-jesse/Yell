ad2b4f1144d84e55b0fc59bd28ef09edc02f4d72
Jesse <83730324+dev-jesse@users.noreply.github.com>
2022-10-29 13:36:20 -0400

    Made the gql mutation "createUsername"

--- frontend/src/components/Auth/Auth.tsx
+++ frontend/src/components/Auth/Auth.tsx
@@ -2,6 +2,8 @@ import { Session } from 'next-auth';
 import { signIn } from 'next-auth/react'
 import { Button, Center, Stack, Text, Image, Input } from '@chakra-ui/react';
 import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import UserOperations from '../../graphql/operations/user'
 
 interface IAuthProps {
     session: Session | null
@@ -14,9 +16,13 @@ const Auth: React.FunctionComponent<IAuthProps> = ({
 }) => {
     const [username, setUsername] = useState('')
 
+    const [createUsername, { data, loading, error }] = useMutation(
+        UserOperations.Mutations.createUsername
+    )
+
     const onSubmit = async () => {
         try {
-
+            await createUsername({ variables: { username } })
         } catch (error) {
             console.log("onSubmit error", error)
         }
